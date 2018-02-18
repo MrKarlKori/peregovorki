dd740ee1a8bacc733c9d2546bf63e1de5b82a131
karlkori <skaller@tut.by>
2018-02-18 15:36:44 +0300

    fixed errors in server side

--- js/queries.js
+++ js/queries.js
@@ -1,6 +1,16 @@
 
-function getUser() {
+function getUsers() {
  fetch('http://localhost:3000/graphql?query={users%20{id}}')
+ .then(function(response) {
+ console.log(response);
+ return response.json();
+ })
+ .then((response)=> console.log(response) )
+ .catch( alert );
+}
+
+function getRooms() {
+ fetch('http://localhost:3000/graphql?query={rooms%20{id, title, capacity, floor}}')
  .then(function(response) {
  console.log(response);
  return response.json();
@@ -11,6 +21,54 @@ function getUser() {
 
 function addUser() {
  fetch('http://localhost:3000/graphql?query=mutation{createUser(input:{login:"Myname",homeFloor:1}){id}}',
+ {
+ method: 'post'
+ }).then(function(response) {
+ console.log(response);
+ return response.json();
+ })
+ .then((response)=> console.log(response) )
+ .catch( alert );
+}
+
+function updateUser() {
+ fetch(`http://localhost:3000/graphql?query=mutation{updateUser(id:4,input:{login:"myName",homeFloor:1}){id, login}}`,
+ {
+ method: 'post'
+ }).then(function(response) {
+ console.log(response);
+ return response.json();
+ })
+ .then((response)=> console.log(response) )
+ .catch( alert );
+}
+
+function addRoom() {
+ fetch(`http://localhost:3000/graphql?query=mutation{createRoom(input:{title:"new room",floor:4,capacity:100}){id}}`,
+ {
+ method: 'post'
+ }).then(function(response) {
+ console.log(response);
+ return response.json();
+ })
+ .then((response)=> console.log(response) )
+ .catch( alert );
+}
+
+function addEvent() {
+ fetch(`http://localhost:3000/graphql?query=mutation{createEvent(input:{title:"new event"}, usersIds: [1], roomId: 3){id}}`,
+ {
+ method: 'post'
+ }).then(function(response) {
+ console.log(response);
+ return response.json();
+ })
+ .then((response)=> console.log(response) )
+ .catch( alert );
+}
+
+function getEvents() {
+ fetch(`http://localhost:3000/graphql?query={events%20{id, title, users{id}, room{id}}}`,
  {
  method: 'post'
  }).then(function(response) {
